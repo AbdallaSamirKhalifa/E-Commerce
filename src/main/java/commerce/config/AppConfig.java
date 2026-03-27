597e0a925c658dd406a97cb550cda8fc47154a51
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-03-27 15:39:58 +0200

    New: SecurityConfig     - Applying basic auth

--- src/main/java/commerce/config/AppConfig.java
+++ src/main/java/commerce/config/AppConfig.java
@@ -1,14 +1,13 @@
 package commerce.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
-    @Bean
-    public JavaMailSender mailSender() {
-        return new JavaMailSenderImpl();
-    }
+//    @Bean
+//    public JavaMailSender mailSender() {
+//        return new JavaMailSenderImpl();
+//    }
 }
