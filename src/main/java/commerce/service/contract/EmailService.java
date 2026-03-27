58382df22ea68adf6cdb78bfe7f88efdb826601a
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-03-27 15:50:42 +0200

    Refactor:     - Sending Email asycronously using Java CompletableFuture

--- src/main/java/commerce/service/contract/EmailService.java
+++ src/main/java/commerce/service/contract/EmailService.java
@@ -1,6 +1,5 @@
 package commerce.service.contract;
 
-import jakarta.mail.MessagingException;
 
 public interface EmailService {
     /**
@@ -9,7 +8,6 @@ public interface EmailService {
      * @param to      the recipient email address
      * @param subject the subject of the email
      * @param text    the body of the email
-     * @throws MessagingException if an error occurs while sending the email
      */
-    void sendEmail(String to, String subject, String text) throws MessagingException;
+    void sendEmailAsync(String to, String subject, String text) ;
 }
--- src/main/java/commerce/service/implementation/GmailService.java
+++ src/main/java/commerce/service/implementation/GmailService.java
@@ -3,29 +3,50 @@ package commerce.service.implementation;
 import commerce.service.contract.EmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class GmailService implements EmailService {
-
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(100, runnable -> {
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.setName("Email");
+        return thread;
+    });
     private final JavaMailSender mailSender;
 
     @Override
-    public void sendEmail(String to, String subject, String text) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
-        helper.setPriority(3);
-        helper.setFrom("no-reply.gmail.com");
-        helper.setSubject(subject);
-        helper.setTo(to);
-        helper.setText(text);
-        mailSender.send(message);
+    public void sendEmailAsync(String to, String subject, String text)  {
+        CompletableFuture.runAsync(() -> {
+            try {
+                MimeMessage message = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+                helper.setPriority(3);
+                helper.setFrom("no-reply.gmail.com");
+                helper.setSubject(subject);
+                helper.setTo(to);
+                helper.setText(text);
+                mailSender.send(message);
+            } catch (MessagingException e) {
+
+            }
+
+        }, EXECUTOR_SERVICE).exceptionally(ex -> {
+            log.error("Error occurred while sending message to {}", to);
+            log.trace("Stack: {}", (Object) ex.getStackTrace());
+            return null;
+        });
+
         log.info("Sending an Email to {}, With Subject {}", to, subject);
     }
 }
