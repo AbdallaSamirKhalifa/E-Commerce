ffc5145f24b86f2863292ecce45c2153657be537
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-03-28 15:38:27 +0200

    Refactor:     - Changing interaction to be between product and category services instead of respository.

--- src/main/java/commerce/service/implementation/ProductService.java
+++ src/main/java/commerce/service/implementation/ProductService.java
@@ -5,9 +5,10 @@ import commerce.dto.response.ProductResponse;
 import commerce.entities.Category;
 import commerce.entities.Product;
 import commerce.exceptions.ResourceNotFoundException;
+import commerce.mappers.contracts.ICategoryMapper;
 import commerce.mappers.contracts.IProductMapper;
-import commerce.repositories.CategoryRepository;
 import commerce.repositories.ProductRepository;
+import commerce.service.contract.ICategoryService;
 import commerce.service.contract.IProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,13 +25,13 @@ import java.util.List;
 @Slf4j
 public class ProductService implements IProductService {
     private final ProductRepository productRepository;
-    private final CategoryRepository categoryRepository;
+    private final ICategoryService categoryService;
     private final IProductMapper productMapper;
+    private final ICategoryMapper categoryMapper;
 
     @Override
     public ProductResponse createProduct(ProductRequest request) {
-        Category category = categoryRepository.findById(request.categoryId())
-                .orElseThrow(() -> new ResourceNotFoundException("Category", request.categoryId()));
+        Category category = categoryMapper.responseToEntity(categoryService.getCategoryById(request.categoryId()));
         Product product = productMapper.requestTooEntity(request);
         product.setCategory(category);
         Product newProduct = productRepository.save(product);
@@ -42,8 +43,7 @@ public class ProductService implements IProductService {
     @Transactional
     @Override
     public ProductResponse updateProduct(ProductRequest request, Integer prodId) {
-        Category category = categoryRepository.findById(request.categoryId())
-                .orElseThrow(() -> new ResourceNotFoundException("Category", request.categoryId()));
+        Category category = categoryMapper.responseToEntity(categoryService.getCategoryById(request.categoryId()));
         Product product = productRepository.findById(prodId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", prodId));
         product.setName(request.name());
