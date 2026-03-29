ab060a999a5d6a1d6aeeda85b2116ca01d72a655
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-03-29 09:02:06 +0200

    Refactor:     - Update ProductUnavailableException

--- src/main/java/commerce/exceptions/ProductUnavailableException.java
+++ src/main/java/commerce/exceptions/ProductUnavailableException.java
@@ -1,7 +1,7 @@
 package commerce.exceptions;
 
 public class ProductUnavailableException extends RuntimeException {
-    public ProductUnavailableException(String message) {
-        super(message);
+    public ProductUnavailableException(String productName) {
+        super(String.format("Product %s is not available at the moment. Please try again later", productName));
     }
 }
