bdb24432d4243802d7b961c026caa843b4d851b5
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-03-29 05:58:59 +0200

    Refactor: New auth role for customer address

--- src/main/java/commerce/security/config/SecurityConfig.java
+++ src/main/java/commerce/security/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package commerce.security.config;
 
+import org.hibernate.SessionCheckMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.SessionManagementDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 public class SecurityConfig {
@@ -21,9 +25,13 @@ public class SecurityConfig {
                                 request.requestMatchers("/api/v1/auth/**").permitAll()
                                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                                         .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
+                                        .requestMatchers("/api/v1/customer/address").hasRole("CUSTOMER")
                                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
+                .sessionManagement(
+                        session->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
