f4bcf1e8bf8f2b4b582f44b021e8b6a6d8e48627
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2026-04-12 18:14:14 +0200

    New: command line runner

--- src/main/java/commerce/ECommerceApplication.java
+++ src/main/java/commerce/ECommerceApplication.java
@@ -9,5 +9,15 @@ public class ECommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
-
+@Bean
+public CommandLineRunner commandLineRunner(RoleRepository repository) {
+    return args -> {
+        if (repository.findRoleByName("ROLE_CUSTOMER").isEmpty())
+        {
+            Role role=new Role();
+            role.setName("ROLE_CUSTOMER");
+            repository.save(role);
+        }
+    };
+}
 }
